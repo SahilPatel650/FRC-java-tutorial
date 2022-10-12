63b7a775fa87c413b5b89f26001ab453773c4d01
Sahil <sahil.va.us@gmail.com>
2022-10-11 22:26:35 -0400

    added robot file

---
+++ robotMap.java
@@ -0,0 +1,19 @@
+public class robotMap {
+    //main function
+    public static void main(String[] args) {
+        //create an array of moves:
+        String[] movesA = {"f", "f", "r", "f"};
+        String[] movesB = {"f", "r", "r", "f"};
+        String[] movesC = {"f", "f", "f", "f", "r", "f","r", "f", "f"};
+
+        mapMoves(movesA);
+        mapMoves(movesB);
+        mapMoves(movesC);
+
+
+    }
+    public static void mapMoves(String[] moves) {
+        
+    }
+
+}
